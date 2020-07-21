6318dcad7ae7bd8f2bf4691c70ebdcc63c25e1ba
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-07-21 09:25:41 -0300

    Update

--- Automated_test/cross_test/cross_test.ino
+++ Automated_test/cross_test/cross_test.ino
@@ -1,12 +1,31 @@
-#define cons 1000
+/*
+this program alows you to generate a signal for the
+cross-correlation circuit and still use your Arduino for
+another things. In other words, this program is runnig 
+in background.
+
+NOTE: never use delay() in another ports of this program.
+*/
+
+#define cons 300
+#define OUT1 10
+#define OUT2 9
+#define CLK 8
 
 String input = "";
 bool LED = 0, mode = 0, doing = 0, said = 1;
+
+//memory
+bool in1[3] = {};
+bool in2[3] = {};
 
 //for make a nonbreak delay
 long unsigned var1 = millis(); 
 long unsigned var2 = var1 + cons;
 long unsigned var3 = var2 + cons;
+long unsigned var4 = var3 + cons;
+long unsigned var5 = var4 + cons;
+long unsigned var6 = var5 + cons;
 
 void generateSignal(void);
 
@@ -19,11 +38,6 @@ void loop() {
     //digitalWrite(LED_BUILTIN, HIGH);
     if (Serial.available())
         input = Serial.readString();
-/*
-    Serial.print(input.length());
-    Serial.print("\t.");
-    Serial.print(input[(input.length() - 1)]);
-*/
 
     if (input == "start\n"){
         //inverter function
@@ -51,32 +65,95 @@ void loop() {
 
     generateSignal();
 
+
+    //Add new code here
+    
+
     delay(1); // to avoid errors
 }
 
 void generateSignal(void){
     if (doing){
-        //create signal
+        //create signal   
         if ((var1 + cons) < millis()){
             var1 = millis();
             //do something
-            Serial.println("step 1");
+            in1[0] = random(2);
+            in2[0] = random(2);
+            //clk low
+            digitalWrite(OUT1, in1[0]);
+            digitalWrite(OUT2, in2[0]);
+            digitalWrite(CLK, LOW);
         }
 
         if ((var2 + cons) < millis()){
             var1 = millis();
             var2 = millis();
-            //do something
-            Serial.println("step 2");
+            //clk high
+            digitalWrite(CLK, HIGH);
         }
 
         if ((var3 + cons) < millis()){
             var1 = millis();
+            var2 = millis();
+            var3 = millis();
+            //do something
+            in1[1] = random(2);
+            in2[1] = random(2);
+            //clk low
+            digitalWrite(OUT1, in1[1]);
+            digitalWrite(OUT2, in2[1]);
+            digitalWrite(CLK, LOW);
+        }
+
+        if ((var4 + cons) < millis()){
+            var1 = millis();
+            var2 = millis();
+            var3 = millis();
+            var4 = millis();
+            //clk high
+            digitalWrite(CLK, HIGH);
+        }
+
+        if ((var5 + cons) < millis()){
+            var1 = millis();
+            var2 = millis();
+            var3 = millis();
+            var4 = millis();
+            var5 = millis();
+            
+            in1[2] = random(2);
+            in2[2] = random(2);
+
+            //clk low
+            digitalWrite(OUT1, in1[2]);
+            digitalWrite(OUT2, in2[2]);
+            digitalWrite(CLK, LOW);
+        }
+
+        if ((var6 + cons) < millis()){
+            var1 = millis(); 
             var2 = var1 + cons;
             var3 = var2 + cons;
-            Serial.println("doing = LOW");
+            var4 = var3 + cons;
+            var5 = var4 + cons;
+            var6 = var5 + cons;
+            
             doing = LOW;
+            //clk high
+            digitalWrite(CLK, HIGH);
+
+            for (int i = 0; i < 3; i++)
+                Serial.print(in1[i]);
+
+            Serial.print("\n");
+
+            for (int i = 0; i < 3; i++)
+                Serial.print(in2[i]);
+
+            Serial.print("\n\n");
         }
+        
     }else if (mode){
         doing = HIGH;
         var1 = millis();
